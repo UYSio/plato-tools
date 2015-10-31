447461cce208fb14336f9c902127f24be210aa83
Juan Matthys Uys <opyate@gmail.com>
2015-10-31 22:57:48 +0000

    showing content location contents

--- hello.rkt
+++ hello.rkt
@@ -1,13 +1,28 @@
 #lang racket
 
+(require xml/xexpr)
+
+
+(define (with-asset path)
+  (printf "path: ~a\n" path)
+  )
+
 ;; Finds Racket sources in all subdirs
-(for ([path (in-directory)]
-      #:when (regexp-match? #rx"[.]rkt$" path))
-  (printf "source file: ~a\n" path))
+(define locs
+  '("/home/opyate/Documents/stuff" "/home/opyate/Documents/stuff2"))
+(for ([loc locs])
+  (for ([path (in-directory loc)])
+
+    (define sub (string-replace (path->string path) loc ""))
+    ;(define out (open-output-file (format "data~a.html" path)))
+    ;(write-xexpr '(div (p path)) out #:insert-newlines? #t)
+    ;(close-output-port out)
+    (with-asset sub)
+    ))
 
 ;; Report each unique line from stdin
-(define seen (make-hash))
-(for ([line (in-lines)])
-  (unless (hash-ref seen line #f)
-    (displayln line))
-  (hash-set! seen line #t))
+; (define seen (make-hash))
+;(for ([line (in-lines)])
+;  (unless (hash-ref seen line #f)
+;    (displayln line))
+;  (hash-set! seen line #t))
