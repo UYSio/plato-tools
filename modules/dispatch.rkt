d34cacb48dffda75bd9679721043da61a7e5666d
Juan Matthys Uys <opyate@gmail.com>
2015-11-01 21:21:30 +0000

    handler

--- modules/dispatch.rkt
+++ modules/dispatch.rkt
@@ -11,11 +11,22 @@
 (define (detect-mime-type asset)
   (detector asset))
 
+(define (base-from-mime-type mime-type)
+  (if mime-type
+      (car (string-split (bytes->string/utf-8 mime-type) "/"))
+      "extension"))
+
+(define (error-handler mime-type)
+  (printf "No handler for mime-type ~a\n" mime-type))
+
 ;; dispatches an asset based on mime-type
 (define (dispatch-asset content-root asset-path out-dir)
-  (let ([mime-type (detect-mime-type asset-path)])
-    (printf "dispatching mime type ~a\n" mime-type )
-    ((hash-ref lookup "image") mime-type)
+  (let* ([mime-type (detect-mime-type asset-path)]
+        [base (base-from-mime-type mime-type)])
+    (if (hash-has-key? lookup base)
+        ((hash-ref lookup base) mime-type)
+        (printf "No handler for ~a\n" mime-type)
+        )
     ))
 
 ;; (define safe (string-replace (get-relative-asset-path content-root asset-path) "/" "_")) ; TODO
