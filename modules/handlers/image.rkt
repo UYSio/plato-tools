57374fcea78a925223e1dd2863a6bdeab71e76d2
Juan Matthys Uys <opyate@gmail.com>
2015-11-02 19:44:15 +0000

    landing-page and entry

--- modules/handlers/image.rkt
+++ modules/handlers/image.rkt
@@ -5,33 +5,17 @@
 (require "../common.rkt"
          "_params.rkt")
 
-;; TODO define
-; asset-landing-page    - the main index.html for the asset
+(define (asset-landing-page params) #f)
 
-
-;; the below s-expr is  quite generic.
-;; what we really want from a handler is to just spit out HTML (xexpr)
-;; and do the necessary post-production (so to speak) to get the assets in
-;; a serve-able form, i.e. photos retouched, and uploaded to S3,
-;; videos equalised and uploaded to S3, etc
-
-(define (asset-listing-snippet
-         content-root
-         asset-path
-         out-dir)
-  (printf "Rendering snippet for ~a\n" asset-path)
-  (let* ([relative-asset-path (get-relative-asset-path content-root asset-path)]
-         [safe (path->dothtml relative-asset-path)])
-    (write-html (format "~a/snippets/~a" out-dir safe) `(div (p ,safe)))))
-
-;; TODO all handlers should be passed two things:
-;; a struct of params: like mime-type, config, asset-path etc
-;; a writer - the writer will know where to put generated content.
-;; The handler should only concern itself with a particular rendetion of
-;; something, and the writer will know where to put it.
-(define (handle-image params)
-  (printf "Handler received params: ~a\n" (p->string params))
+(define (asset-entry params)
   (let ([content-root (p-content-root params)]
         [asset-path (p-asset-path params)]
         [out-dir (p-out-dir params)])
-    (asset-listing-snippet content-root asset-path out-dir)))
+    (let* ([relative-asset-path (get-relative-asset-path content-root asset-path)]
+           [safe (path->dothtml relative-asset-path)])
+      (write-html (format "~a/snippets/~a" out-dir safe) `(div (p ,safe))))))
+
+(define (handle-image params)
+  (printf "Handler=[image], params: ~a\n" (p->string params))
+  (asset-landing-page params)
+  (asset-entry params))
