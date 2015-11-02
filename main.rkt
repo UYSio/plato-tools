1509da5df627f61db363202d24733db57c0395a5
Juan Matthys Uys <opyate@gmail.com>
2015-11-02 22:45:46 +0000

    use racket/path instead of custom

--- main.rkt
+++ main.rkt
@@ -1,23 +1,18 @@
 #lang racket
+
+(require racket/path)
 (require "modules/dispatch.rkt")
 
 (require (file "~/.plato/config.rkt"))
 (define asset-roots (hash-ref plato-opts 'asset-roots))
 (define out-dir (hash-ref plato-opts 'out-dir))
 
-
-;; given a content-root (e.g. /a/b/c)
-;; given an asset-path (e.g. /a/b/c/d/asset.ext)
-;; return the difference (e.g. d/asset.ext)
-(define (get-relative-asset-path content-root asset-path)
-  (string-replace (path->string asset-path) content-root ""))
-
 ;; Finds Racket sources in all subdirs
 (for ([asset-root-s asset-roots])
   ;; add a trailing slash, even if already
   (let ([asset-root (string-append asset-root-s "/")])
     (for ([abs-path (in-directory asset-root)])
-      (let ([rel-path (get-relative-asset-path asset-root abs-path)])
+      (let ([rel-path (path->string (find-relative-path asset-root abs-path))])
         (if (file-exists? abs-path)
             (dispatch-asset abs-path rel-path out-dir)
             #f)))))
