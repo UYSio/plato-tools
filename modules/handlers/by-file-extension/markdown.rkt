d8f0aafd39ce44ca450b9dd74257fd6854b2e31b
Juan Matthys Uys <opyate@gmail.com>
2015-11-08 21:25:48 +0000

    get started with front matter

--- modules/handlers/by-file-extension/markdown.rkt
+++ modules/handlers/by-file-extension/markdown.rkt
@@ -6,6 +6,30 @@
          "../_params.rkt"
          "../../../lib/hoedown.rkt")
 
+(define (lp2 markdown front-matter)
+  (printf "MD: ~a\n" markdown)
+  (printf "FM: ~a\n" front-matter))
+
+; Given, e.g. "layout: post" or "layout:post"
+; returns '("layout" "post")
+(define (parse-front-matter-item line)
+  (cdr (regexp-match #rx"^(.*): ?(.*)$" line)))
+
+(define (parse-front-matter markdown-file)
+  (let ([lines (file->lines markdown-file)]
+        [front-matter (make-hash)]
+        [markdown '()]
+        ; front-matter is at top, so start with is-content? #f
+        [is-content? #f])
+    (for ([line lines]
+          #:unless (and (equal? line "---")
+                        (set! is-content? #t)))
+      (if is-content?
+          ; TODO foldl the markdown
+          (set! markdown (append markdown (list line)))
+          (let ([fm (parse-front-matter-item line)])
+            (dict-set! front-matter (first fm) (second fm)))))
+    (lp2 markdown front-matter)))
 
 (define (lp params)
   (let* ([markdown-file (p-asset-path params)]
@@ -23,5 +47,7 @@
 
 (define (handle-markdown params)
   (printf "Handler=[markdown], params: ~a\n" (p->string params))
-  (lp params)
+  ;(lp params)
+  (let ([markdown-file (p-asset-path params)])
+    (parse-front-matter markdown-file))
   (asset-entry params))
