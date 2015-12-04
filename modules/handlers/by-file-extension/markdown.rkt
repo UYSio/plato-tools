392f184477ed322af41422b6baab5d7898ba4976
Juan Matthys Uys <opyate@gmail.com>
2015-12-04 22:43:48 +0000

    markdown and frontmatter

--- modules/handlers/by-file-extension/markdown.rkt
+++ modules/handlers/by-file-extension/markdown.rkt
@@ -6,34 +6,36 @@
          "../_params.rkt"
          "../../../lib/hoedown.rkt")
 
-(define (lp2 markdown front-matter)
-  (printf "MD: ~a\n" markdown)
-  (printf "FM: ~a\n" front-matter))
-
 ; Given, e.g. "layout: post" or "layout:post"
 ; returns '("layout" "post")
 (define (parse-front-matter-item line)
   (cdr (regexp-match #rx"^(.*): ?(.*)$" line)))
 
-(define (parse-front-matter markdown-file)
+; parses out markdown-str and front-matter
+(define (parse markdown-file)
   (let ([lines (file->lines markdown-file)]
         [front-matter (make-hash)]
-        [markdown '()]
+        [markdown-list '()]
         ; front-matter is at top, so start with is-content? #f
         [is-content? #f])
     (for ([line lines]
           #:unless (and (equal? line "---")
                         (set! is-content? #t)))
       (if is-content?
-          ; TODO foldl the markdown
-          (set! markdown (append markdown (list line)))
+          (set! markdown-list (append markdown-list (list line)))
           (let ([fm (parse-front-matter-item line)])
             (dict-set! front-matter (first fm) (second fm)))))
-    (lp2 markdown front-matter)))
+    (let ([markdown-str (foldl (lambda (x acc) (format "~a\n~a" acc x)) "" markdown-list)])
+      ;(printf "MD: ~a\n" (markdown markdown-str))
+      ;(printf "FM: ~a\n" front-matter)
+      (list markdown-str front-matter))))
 
 (define (lp params)
   (let* ([markdown-file (p-asset-path params)]
-        [markdown-str (file->string markdown-file)]
+        ;[markdown-str (file->string markdown-file)]
+         [parsed (parse markdown-file)]
+         [markdown-str (first parsed)]
+         [front-matter (second parsed)]
         [html (markdown markdown-str)])
     (let ([landing-page-dir (p-landing-page-dir params)])
       (display-to-file
@@ -47,7 +49,6 @@
 
 (define (handle-markdown params)
   (printf "Handler=[markdown], params: ~a\n" (p->string params))
-  ;(lp params)
-  (let ([markdown-file (p-asset-path params)])
-    (parse-front-matter markdown-file))
+  (lp params)
+  ;(let ([markdown-file (p-asset-path params)]) (parse-front-matter markdown-file))
   (asset-entry params))
