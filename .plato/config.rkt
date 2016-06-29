46676bba05fa9f5d5db9ab7b0f5590c3b3ac8705
Juan M. Uys <opyate@gmail.com>
2016-06-29 23:20:47 +0100

    photo examples

--- .plato/config.rkt
+++ .plato/config.rkt
@@ -4,7 +4,7 @@
 
 ; configuration
 (define cfg (make-hash))
-(dict-set! cfg 'input-roots '("LOC1" "LOC2" "LOC3" "LOC4"))
+(dict-set! cfg 'input-roots '("LOC1" "LOC2" "LOC3" "LOC4" "LOC5"))
 (dict-set! cfg 'output-root "OUT_ROOT")
 (dict-set! cfg 'output-entries "OUT_ENTRIES")
 (dict-set! cfg 'skip ".plato-skip")
--- scripts/prep.sh
+++ scripts/prep.sh
@@ -20,6 +20,7 @@ fresh() {
 	perl -pi -e 's/LOC2/$ENV{LOC2}/g' $CONF/config.rkt
 	perl -pi -e 's/LOC3/$ENV{LOC3}/g' $CONF/config.rkt
 	perl -pi -e 's/LOC4/$ENV{LOC4}/g' $CONF/config.rkt
+	perl -pi -e 's/LOC5/$ENV{LOC5}/g' $CONF/config.rkt
 	perl -pi -e 's/OUT_ROOT/$ENV{OUT_ROOT}/g' $CONF/config.rkt
 	perl -pi -e 's/OUT_ENTRIES/$ENV{OUT_ENTRIES}/g' $CONF/config.rkt
 }
